cc4e335984c3ca14df321e5f13cbf7bdbdc43c2c
innovention <sbulkley@zagmail.gonzaga.edu>
2015-09-21 20:24:58 -0700

    Endpoints, errors, nakedness.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var url     = require('url');
+var fs      = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,8 +13,31 @@ app.get('/',function(req,res){
   res.sendFile('index.html');
 });
 
-app.get('/info',function(req,res){
+app.get('/listen',function(req,res){
   res.sendFile(path.join(__dirname, '/info', 'info.json'));
+});
+
+app.get('/talk',function(req,res){
+  var uri = url.parse(req.url, true);
+  var query = uri.query;
+  if(query.key === process.env.KEY) {
+    var obj = {};
+    obj.temperature_one = query.t1  || 0;
+    obj.temperature_two = query.t2  || 0;
+    obj.moisture        = query.mst || 0;
+    obj.water           = query.wtr || 0;
+    obj.time            = query.tme || 0;
+    fs.writeFile(path.join(__dirname, '/info', 'info.json'), JSON.stringify(obj), function(err) {
+      if(err) {
+        res.sendStatus(417);
+        return console.log(err);
+      }
+      res.sendStatus(200);
+      console.log("+HG: it is written.");
+    });
+  } else {
+    res.sendStatus(403);
+  } 
 });
 
 app.listen(app.get('port'), function() {
