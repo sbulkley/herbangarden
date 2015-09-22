71936faff6a41cc777e3e386f7aba128f6d67e60
innovention <sbulkley@zagmail.gonzaga.edu>
2015-09-21 19:11:26 -0700

    Changed to JSON, added endpoint

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require("express");
 var app     = express();
+var path    = require("path");
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/pages'));
-
 app.use(express.static(__dirname + '/scripts'));
 
 app.get('/',function(req,res){
   res.sendFile('index.html');
+});
+
+app.get('/info',function(req,res){
+  res.sendFile(path.join(__dirname, '/info', 'info.json'));
 });
 
 app.listen(app.get('port'), function() {
